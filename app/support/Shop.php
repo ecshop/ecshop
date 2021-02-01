e0e57dee730fe5a62cbab0b75633a1b852efe9d4
deno <inbox@email.com>
2021-02-01 18:12:36 +0800

    更新Shop类

--- app/support/Shop.php
+++ app/support/Shop.php
@@ -4,124 +4,27 @@ namespace app\support;
 
 class Shop
 {
-    public $db_name = '';
-    public $prefix = 'ecs_';
-
     /**
-     * Shop constructor.
-     * @param $db_name
-     * @param $prefix
-     */
-    public function __construct($db_name, $prefix)
-    {
-        $this->db_name = $db_name;
-        $this->prefix = $prefix;
-    }
-
-    /**
-     * 将指定的表名加上前缀后返回
-     *
-     * @access  public
-     * @param string $str 表名
-     *
-     * @return  string
-     */
-    public function table($str)
-    {
-        return '`' . $this->db_name . '`.`' . $this->prefix . $str . '`';
-    }
-
-    /**
-     * ECSHOP 密码编译方法;
-     *
-     * @access  public
-     * @param string $pass 需要编译的原始密码
-     *
-     * @return  string
-     */
-    public function compile_password($pass)
-    {
-        return md5($pass);
-    }
-
-    /**
-     * 取得当前的域名
-     *
-     * @access  public
-     *
-     * @return  string      当前的域名
-     */
-    public function get_domain()
-    {
-        /* 协议 */
-        $protocol = $this->http();
-
-        /* 域名或IP地址 */
-        if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
-            $host = $_SERVER['HTTP_X_FORWARDED_HOST'];
-        } elseif (isset($_SERVER['HTTP_HOST'])) {
-            $host = $_SERVER['HTTP_HOST'];
-        } else {
-            /* 端口 */
-            if (isset($_SERVER['SERVER_PORT'])) {
-                $port = ':' . $_SERVER['SERVER_PORT'];
-
-                if ((':80' == $port && 'http://' == $protocol) || (':443' == $port && 'https://' == $protocol)) {
-                    $port = '';
-                }
-            } else {
-                $port = '';
-            }
-
-            if (isset($_SERVER['SERVER_NAME'])) {
-                $host = $_SERVER['SERVER_NAME'] . $port;
-            } elseif (isset($_SERVER['SERVER_ADDR'])) {
-                $host = $_SERVER['SERVER_ADDR'] . $port;
-            }
-        }
-
-        return $protocol . $host;
-    }
-
-    /**
-     * 获得 ECSHOP 当前环境的 URL 地址
-     *
-     * @access  public
-     *
-     * @return  void
+     * 获得当前环境的 URL 地址
+     * @return string
      */
     public function url()
     {
-        $curr = strpos(PHP_SELF, ADMIN_PATH . '/') !== false ?
-            preg_replace('/(.*)(' . ADMIN_PATH . ')(\/?)(.)*/i', '\1', dirname(PHP_SELF)) :
-            dirname(PHP_SELF);
-
-        $root = str_replace('\\', '/', $curr);
-
-        if (substr($root, -1) != '/') {
-            $root .= '/';
-        }
-
-        return $this->get_domain() . $root;
+        return request()->rootUrl();
     }
 
     /**
-     * 获得 ECSHOP 当前环境的 HTTP 协议方式
-     *
-     * @access  public
-     *
-     * @return  void
+     * 获得当前环境的 HTTP 协议方式
+     * @return  string
      */
     public function http()
     {
-        return (isset($_SERVER['HTTPS']) && (strtolower($_SERVER['HTTPS']) != 'off')) ? 'https://' : 'http://';
+        return request()->scheme() . '://';
     }
 
     /**
      * 获得数据目录的路径
-     *
      * @param int $sid
-     *
      * @return string 路径
      */
     public function data_dir($sid = 0)
@@ -138,9 +41,7 @@ class Shop
 
     /**
      * 获得图片的目录路径
-     *
      * @param int $sid
-     *
      * @return string 路径
      */
     public function image_dir($sid = 0)

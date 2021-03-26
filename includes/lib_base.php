fc9d155724f04368e0e349eb828e20ad7cbf6f60
deno <inbox@email.com>
2021-03-26 15:22:36 +0800

    增加机设备判断

--- includes/lib_base.php
+++ includes/lib_base.php
@@ -900,3 +900,22 @@ function write_static_cache($cache_name, $caches)
     $content .= "?>";
     file_put_contents($cache_file_path, $content, LOCK_EX);
 }
+
+/**
+ * 检测是否使用手机访问
+ * @return bool
+ */
+function is_mobile()
+{
+    if (isset($_SERVER['HTTP_VIA']) && stristr($_SERVER['HTTP_VIA'], "wap")) {
+        return true;
+    } elseif (isset($_SERVER['HTTP_ACCEPT']) && strpos(strtoupper($_SERVER['HTTP_ACCEPT']), "VND.WAP.WML")) {
+        return true;
+    } elseif (isset($_SERVER['HTTP_X_WAP_PROFILE']) || isset($_SERVER['HTTP_PROFILE'])) {
+        return true;
+    } elseif (isset($_SERVER['HTTP_USER_AGENT']) && preg_match('/(blackberry|configuration\/cldc|hp |hp-|htc |htc_|htc-|iemobile|kindle|midp|mmp|motorola|mobile|nokia|opera mini|opera |Googlebot-Mobile|YahooSeeker\/M1A1-R2D2|android|iphone|ipod|mobi|palm|palmos|pocket|portalmmm|ppc;|smartphone|sonyericsson|sqh|spv|symbian|treo|up.browser|up.link|vodafone|windows ce|xda |xda_)/i', $_SERVER['HTTP_USER_AGENT'])) {
+        return true;
+    }
+
+    return false;
+}

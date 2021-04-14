1da197dc751f902fd38fbb0f3809c62e2652995b
deno <inbox@email.com>
2021-04-14 09:11:40 +0800

    更新PHP运行时版本

--- includes/inc_constant.php
+++ includes/inc_constant.php
@@ -4,11 +4,11 @@ if (!defined('IN_ECS')) {
     die('Hacking attempt');
 }
 
-if (version_compare(PHP_VERSION, '7.4.0', '<') || version_compare(PHP_VERSION, '8.0.0', '>=')) {
-    die('Require a PHP version "7.4". You are running ' . PHP_VERSION . '.');
+if (version_compare(PHP_VERSION, '8.0.0', '<')) {
+    die('Require a PHP version "8.0". You are running ' . PHP_VERSION . '.');
 }
 
-if (in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
+if (in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
     defined('DEBUG_MODE') or define('DEBUG_MODE', true);
 } else {
     defined('DEBUG_MODE') or define('DEBUG_MODE', false);

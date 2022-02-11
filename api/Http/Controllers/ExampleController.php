82c11bea91120ceb32e15ab27aa26028f1910a8c
deno <inbox@email.com>
2022-02-11 10:04:25 +0800

    add api interface

---
+++ api/Http/Controllers/ExampleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ExampleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function index()
+    {
+        return 'ExampleController';
+    }
+}

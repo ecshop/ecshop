cd9d43e2149c4c82e1a1d760b9f304d795fc84bb
deno <inbox@email.com>
2022-02-14 14:19:15 +0800

    remove

--- includes/inc_constant.php
+++ includes/inc_constant.php
@@ -14,8 +14,6 @@ if (in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
     defined('DEBUG_MODE') or define('DEBUG_MODE', false);
 }
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
 define('APPNAME', 'Ecshop');
 define('VERSION', 'v2.9.0-dev');
 define('RELEASE', '20220211');

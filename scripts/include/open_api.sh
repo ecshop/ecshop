f5aa415948d4c082f00bf53fd432242db18328c1
deno <inbox@email.com>
2023-09-07 17:51:38 +0800

    优化代码

--- scripts/include/open_api.sh
+++ scripts/include/open_api.sh
@@ -2,20 +2,8 @@ Echo_Green '------------------------------'
 Echo_Green ' 生成swagger接口文档'
 Echo_Green '------------------------------'
 
-vendor/bin/openapi app/Http/Auth/ app/Entities/ \
-  -o public/swagger/auth.json -f json
-
-vendor/bin/openapi app/Http/Manager/ app/Entities/ \
-  $(Get_Bundles "Manager") \
-  -o public/swagger/manager.json -f json
-
-vendor/bin/openapi app/Http/User/ app/Entities/ \
-  $(Get_Bundles "User") \
-  -o public/swagger/user.json -f json
-
-vendor/bin/openapi app/Http/Portal/ app/Entities/ \
-  $(Get_Bundles "Portal") \
-  -o public/swagger/portal.json -f json
+vendor/bin/openapi $(Get_Bundles "Api") \
+  -o public/swagger/api.json -f json
 
 Echo_Green '------------------------------'
 Echo_Green ' 生成typescript接口'

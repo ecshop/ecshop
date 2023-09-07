1d34f1733ad2c341dc45d3918a15832cba19d7d0
deno <inbox@email.com>
2023-09-07 18:05:15 +0800

    优化代码

--- app/Bundles/Seller/Routes/web.php
+++ app/Bundles/Seller/Routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->group(function () {
-    Route::get('/', ['\App\Bundles\Seller\Controllers\IndexController::class', 'index']);
+    Route::get('/', [\App\Bundles\Seller\Controllers\IndexController::class, 'index']);
 });

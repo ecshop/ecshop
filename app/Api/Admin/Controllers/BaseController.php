84080aed8eeaed1d05053aee56a928a4aeff7be8
deno <inbox@email.com>
2025-12-11 18:27:40 +0800

    docs: 更新API模块注解

--- app/Api/Admin/Controllers/BaseController.php
+++ app/Api/Admin/Controllers/BaseController.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace App\Api\Admin\Controllers;
 
+use App\Constants\Constant;
 use App\Http\Controllers\Controller;
+use OpenApi\Attributes as OA;
+use OpenApi\Attributes\Contact;
 
+#[OA\Info(version: Constant::Version, description: Constant::Release, title: '管理平台API文档', contact: new Contact('API Develop Team'))]
+#[OA\Server(url: '/api/admin/', description: '开发环境')]
+#[OA\SecurityScheme(securityScheme: 'bearerAuth', type: 'http', description: 'JWT 认证信息', name: 'Authorization', in: 'header', bearerFormat: 'JWT', scheme: 'bearer')]
 abstract class BaseController extends Controller
 {
 
--- app/Api/User/Controllers/BaseController.php
+++ app/Api/User/Controllers/BaseController.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace App\Api\User\Controllers;
 
+use App\Constants\Constant;
 use App\Http\Controllers\Controller;
+use OpenApi\Attributes as OA;
+use OpenApi\Attributes\Contact;
 
+#[OA\Info(version: Constant::Version, description: Constant::Release, title: '会员API文档', contact: new Contact('API Develop Team'))]
+#[OA\Server(url: '/api/user/', description: '开发环境')]
+#[OA\SecurityScheme(securityScheme: 'bearerAuth', type: 'http', description: 'JWT 认证信息', name: 'Authorization', in: 'header', bearerFormat: 'JWT', scheme: 'bearer')]
 abstract class BaseController extends Controller
 {
 
